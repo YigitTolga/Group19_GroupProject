35f7e39ceac0a0ec6b3168b55a58e8100d96d199
Zebib Teweldebrhan <zizu_ss@yahoo.com>
2021-07-07 20:16:43 -0400

    Work one is done
    Working progress

--- src/test/java/Zebib/Hello.java
+++ src/test/java/Zebib/Hello.java
@@ -2,6 +2,8 @@ package Zebib;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello guys!");
+        System.out.println("Hello guys!!");
+        // group work has started
+        //
     }
 }
