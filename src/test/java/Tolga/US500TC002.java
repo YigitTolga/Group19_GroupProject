90a498ad0ccc64fb8108165c559d6d5faa5eea47
Jorge Flores <jorgeareszuma86@gmail.com>
2021-07-07 19:17:34 -0500

    US500TC00 second commit
    US500TC002 First commit

---
+++ src/test/java/Tolga/US500TC002.java
@@ -0,0 +1,9 @@
+package Tolga;
+
+public class US500TC002 {
+    public static void main(String[] args) {
+        // work has started with cucumber
+        // we are getting closer
+        // i haven't push yet
+    }
+}
