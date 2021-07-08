7b306291e8d3deb5959aa78c9e752397b7df42d7
Bashir Hasanov <beshir.hasanov@gmail.com>
2021-07-07 20:18:52 -0400

    Bashir - coban salati

---
+++ src/test/java/Bashir/US500TC002Bashir.java
@@ -0,0 +1,6 @@
+package Bashir;
+
+public class US500TC002Bashir {
+    // work has started with a lot of hiyar, badimcan, biber and yasil sogan
+    // now we can add zeytun yagi and eat the salad!
+}
